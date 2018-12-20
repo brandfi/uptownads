1e3c30dd21398c9a6e7414508b8d066b0bae0d8e
brandfi <joenjuguna53@gmail.com>
2018-12-20 18:35:53 +0300

    Fix js bug.

--- dashboard/static/dashboard/js/graphs.js
+++ dashboard/static/dashboard/js/graphs.js
@@ -21,7 +21,9 @@ $(document).ready(function () {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.json("http://127.0.0.1:8000/dashboard/impressions-api/").then(function (data) {
+    var url = "http://" + window.location.hostname + '/dashboard/impressions-api/';
+
+    d3.json(url).then(function (data) {
         var jsonData = JSON.parse(data);
         jsonData.forEach(function (d) {
             d.title = d.title;
@@ -44,7 +46,7 @@ $(document).ready(function () {
 
         // text label for the x axis
         svg.append("text")
-            .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 10) + ")")
+            .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 20) + ")")
             .style("text-anchor", "middle")
             .attr("font", "sans-serif")
             .attr("font-size", "12px")
